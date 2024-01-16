b3a8d901f44e1f2f795f5e03f6dc34708a2d8f5f
ShreyaWanjari <shreyawanjari2001@gmail.com>
2024-01-16 10:32:53 +0530

    Added Scroll View to Cards

--- Memorize Game/ContentView.swift
+++ Memorize Game/ContentView.swift
@@ -12,40 +12,55 @@ struct ContentView: View {
     @State var CardCount : Int = 4
     var body: some View {
         VStack{
-            HStack{
-                ForEach(0..<CardCount , id : \.self  ){ item in
-                    CardView(content: emojis[item] )
-                }
-            }.foregroundColor(.red)
-            HStack{
-                Button(action: {
-                    if CardCount < emojis.count{
-                        CardCount += 1
-                    }
-                  
-                },label: {
-                    Image(systemName: "plus.circle.fill")
-                })
-            
+            ScrollView{
+                card
+            }
                 Spacer()
-                Button(action: {
-                    if CardCount > 1{
-                        CardCount -= 1
-                    }else{
-                        
-                    }
-                   
-                },label: {
-                    Image(systemName: "minus.circle.fill")
-                })
-            }.imageScale(.large)
-                .font(.largeTitle)
-          
+                cardCountADjuster
+            
         }
       
         .padding()
         
     }
+    
+    var cardCountADjuster : some View{
+        HStack{
+        cardAdder
+        Spacer()
+        cardRemover
+        }.imageScale(.large)
+         .font(.largeTitle)
+    }
+    var card : some View{
+        LazyVGrid(columns : [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<CardCount , id : \.self  ){ item in
+                CardView(content: emojis[item] )
+                    .aspectRatio(2/3 , contentMode: .fit)
+            }
+        }.foregroundColor(.red)
+    }
+    var cardRemover : some View{
+        Button(action: {
+            if CardCount > 1{
+                CardCount -= 1
+            }
+        },label: {
+            Image(systemName: "minus.circle.fill")
+        }).disabled(CardCount <= 1 )
+    }
+    
+    var cardAdder : some View{
+            Button(action: {
+                if CardCount < emojis.count{
+                    CardCount += 1
+                }
+              
+            },label: {
+                Image(systemName: "plus.circle.fill")
+            }).disabled(CardCount >= emojis.count)
+    }
+    
 }
 
 struct CardView : View{
@@ -55,13 +70,14 @@ struct CardView : View{
     var base : RoundedRectangle = RoundedRectangle(cornerRadius: 12)
     var body: some View{
         ZStack{
-            if isFaceup {
+         Group {
                 base.foregroundColor(.white)
                  base.stroke(lineWidth: 3)
                  Text(content).font(.largeTitle)
-            }else{
-                base.fill()
             }
+         .opacity(isFaceup ? 1 : 0)
+                base.fill().opacity(isFaceup ? 0 : 1)
+            
        
         }.onTapGesture {
             isFaceup.toggle()
